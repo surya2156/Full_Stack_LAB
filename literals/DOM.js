62e0ebc3b408188465d63a58c550c20041a6fd96
surya2156 <suryagoayl156@gmail.com>
2025-10-14 15:25:03 +0530

    ok

---
+++ literals/DOM.js
@@ -0,0 +1,7 @@
+const div=document.createElement("div");
+const h1=document.createElement("h1");
+h1.innerText="python is create ai";
+div.appendChild(h1);
+
+const id=document.getElementById("root");
+id.appendChild(div);
